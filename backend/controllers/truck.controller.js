5a5697c5f53f281c0046895ae04da2d0487dbf35
Benjamin Harvey <benjaminharvey2000@gmail.com>
2026-02-09 20:30:27 +1030

    Truck route

--- backend/controllers/truck.controller.js
+++ backend/controllers/truck.controller.js
@@ -1,4 +1,7 @@
 import Truck from "../models/truck.model.js";
+import TruckModel from "../models/truckModel.model.js";
+import Customer from "../models/customer.model.js";
+import User from "../models/user.model.js";
 
 export async function getAllTrucks(req, res) {
     try {
@@ -22,8 +25,22 @@ export async function getTruckById(req, res) {
 
 export async function createTruck(req, res) {
     try {
-        const {model, customer, salesman, chassis, stock, registration} = req.body;
+        const {model, customer, salesman, chassis, stock, registration} = req.validatedData;
         
+        const foundModel = await TruckModel.findById(model);
+        if (!foundModel) return res.status(400).json({message: "Model does not exist"});
+
+        const foundCustomer = await Customer.findById(customer);
+        if (!foundCustomer) return res.status(400).json({message: "Customer does not exist"});
+
+        const foundSalesman = await User.findById(salesman);
+        if (!foundSalesman) return res.status(400).json({message: "Salesman does not exist"});
+
+        const newTruck = new Truck({model, customer, salesman, chassis, stock, registration});
+        await newTruck.save();
+
+        return res.status(201).json({message:"Successfully created new truck", truck:newTruck});
+
     } catch (error) {
         console.log("ERROR: createTruck @ truck controller");
         throw new Error("Internal Server Error", 500);
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import { signup, login, logout, checkAuth } from "../controllers/auth.controller.js";
 import {protectRoute} from "../middleware/protectRoute.js";
 import { validate } from "../middleware/validationMiddleware.js";
-import { loginSchema, signupSchema } from "../schemas/schemas.js";
+import { loginSchema, signupSchema, createTruckSchema } from "../schemas/schemas.js";
 
 const router = express.Router();
 
@@ -10,6 +10,6 @@ router.get("/check-auth", protectRoute, checkAuth);
 
 router.post("/signup", validate(signupSchema), signup);
 router.post("/login", validate(loginSchema), login);
-router.post("/logout", logout);
+router.post("/logout", validate(createTruckSchema), logout);
 
 export default router;
